2193bb107845ec404302fd0ef4248fdfc5399154
daniffig <daniffig@gmail.com>
2013-10-30 01:01:09 -0300

    Agrego atributos y métodos básicos a la clase Game.

--- JBomb/src/core/Game.java
+++ JBomb/src/core/Game.java
@@ -1,5 +1,55 @@
 package core;
 
+import java.util.List;
+
 public class Game {
 
+	private List<Player> Players;
+	private Integer MaxRounds;
+	private Integer CurrentRound;
+	private Integer MaxPlayersAllowed;
+
+	public List<Player> getPlayers() {
+		return Players;
+	}
+
+	public void setPlayers(List<Player> players) {
+		Players = players;
+	}
+
+	public Integer getMaxRounds() {
+		return MaxRounds;
+	}
+
+	public void setMaxRounds(Integer maxRounds) {
+		MaxRounds = maxRounds;
+	}
+
+	public Integer getCurrentRound() {
+		return CurrentRound;
+	}
+
+	public void setCurrentRound(Integer currentRound) {
+		CurrentRound = currentRound;
+	}
+
+	public Integer getMaxPlayersAllowed() {
+		return MaxPlayersAllowed;
+	}
+
+	public void setMaxPlayersAllowed(Integer maxPlayersAllowed) {
+		MaxPlayersAllowed = maxPlayersAllowed;
+	}
+	
+	public Boolean addPlayer(Player p)
+	{
+		if (this.getPlayers().size() < this.getMaxPlayersAllowed())
+		{
+			this.getPlayers().add(p);
+			
+			return true;
+		}
+		
+		return false;
+	}
 }
