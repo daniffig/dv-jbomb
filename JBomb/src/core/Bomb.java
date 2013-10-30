bdd2f7d5443f8fb66bab1de0a26075b7dc4f3731
daniffig <daniffig@gmail.com>
2013-10-30 01:17:32 -0300

    Agrego dos atributos a la clase Bomb.

--- JBomb/src/core/Bomb.java
+++ JBomb/src/core/Bomb.java
@@ -2,4 +2,19 @@ package core;
 
 public class Bomb {
 
+	private Integer DetonationMilliseconds;
+	private Integer CurrentMilliseconds;
+	
+	public Integer getDetonationMilliseconds() {
+		return DetonationMilliseconds;
+	}
+	public void setDetonationMilliseconds(Integer detonationMilliseconds) {
+		DetonationMilliseconds = detonationMilliseconds;
+	}
+	public Integer getCurrentMilliseconds() {
+		return CurrentMilliseconds;
+	}
+	public void setCurrentMilliseconds(Integer currentMilliseconds) {
+		CurrentMilliseconds = currentMilliseconds;
+	}
 }
