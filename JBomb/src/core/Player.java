a4dac9cb71440c6173e07b27eee573927e4f8199
daniffig <daniffig@gmail.com>
2013-10-30 02:08:57 -0300

    Agrego el atributo PlayerName a la clase Player.

--- JBomb/src/core/Player.java
+++ JBomb/src/core/Player.java
@@ -2,4 +2,13 @@ package core;
 
 public class Player {
 
+	private String PlayerName;
+
+	public String getPlayerName() {
+		return PlayerName;
+	}
+
+	public void setPlayerName(String playerName) {
+		PlayerName = playerName;
+	}
 }
