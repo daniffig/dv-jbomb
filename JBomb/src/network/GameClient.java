e2175b11e59f5c772cd4c2c2a3483c013e9b077a
Andres1433 <Andres1433@gmail.com>
2013-11-26 20:19:36 -0300

    Agregada logica para cargar archivo de configuraciones

--- JBomb/src/network/GameClient.java
+++ JBomb/src/network/GameClient.java
@@ -12,10 +12,13 @@ public class GameClient {
 	public String server_ip;
 	public String server_port;
 	
-	public static void main(String[] args) {
-		
-		GameClient game_client = new GameClient();
-		
+	public GameClient()
+	{
+		this.readConfigurationFile();
+	}
+	
+	public void readConfigurationFile()
+	{
 		String configuration_file = "";
 
 		if(GameClient.class.getAnnotation(Server.class) != null)
@@ -34,19 +37,21 @@ public class GameClient {
 			while(scanner.hasNext())
 			{
 				key = scanner.next();
-				if(key.equals("ipServer"))
-					game_client.server_ip = scanner.next();
-				if(key.equals("portServer"))
-					game_client.server_port = scanner.next();
+				if(key.equals("ipServer")) this.server_ip = scanner.next();
+				else if(key.equals("portServer"))this.server_port = scanner.next();
 			}
 			scanner.close();
 		}
 		catch(FileNotFoundException e){
-			System.out.println("El archivo especificado en la anotacion @server() no encontrado");
+			System.out.println("Archivo especificado en la anotacion @server() no encontrado");
 			System.exit(-1);
 		}
 		
-		System.out.println("Servidor " + game_client.server_ip + ":" + game_client.server_port);
+		System.out.println("Servidor " + this.server_ip + ":" + this.server_port);
+	}
+	
+	public static void main(String[] args) {
+
 	}
 
 }
