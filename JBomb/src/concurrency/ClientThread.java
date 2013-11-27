ea8554539c71be927921398efe7c13a5676b66c9
Andres1433 <Andres1433@gmail.com>
2013-11-26 22:17:03 -0300

    Conectado el servidor con el objeto juego

--- JBomb/src/concurrency/ClientThread.java
+++ JBomb/src/concurrency/ClientThread.java
@@ -2,13 +2,17 @@ package concurrency;
 
 import java.net.Socket;
 
+import core.Game;
+
 public class ClientThread implements Runnable {
 
 	private Socket client_socket;
+	private Game current_game;
 	
-	public ClientThread(Socket s)
+	public ClientThread(Socket s, Game g)
 	{
 		this.client_socket = s;
+		this.current_game = g;
 	}
 	@Override
 	public void run() {
--- JBomb/src/network/GameServer.java
+++ JBomb/src/network/GameServer.java
@@ -5,16 +5,39 @@ import java.io.IOException;
 import java.net.ServerSocket;
 
 import concurrency.ClientThread;
+import core.Game;
 
 public class GameServer {
 
+	private Game current_game;
+	
+	public GameServer()
+	{
+		current_game = new Game();
+		current_game.setInetPort(6621);
+		current_game.setName("JBomb!");
+		current_game.setMaxRounds(2);
+		current_game.setMaxGamePlayersAllowed(10);
+	}
+	
+	public Game getGame()
+	{
+		return this.current_game;
+	}
+	
+	public void setGame(Game g)
+	{
+		this.current_game = g;
+	}
+	
 	public static void main(String[] args)
 	{
+		GameServer game_server = new GameServer();
 		ServerSocket server = null;
 		
 		try
 		{ 
-			server = new ServerSocket(6621);
+			server = new ServerSocket(game_server.getGame().getInetPort());
 		} 
 		catch (IOException e)
 		{
@@ -22,13 +45,13 @@ public class GameServer {
 			System.exit(-1);
 		}
 		
-		while(true)
+		while(game_server.getGame().canAddPlayer())
 		{
 			if(server != null)
 			{
 				try
 				{
-					ClientThread stp = new ClientThread(server.accept());
+					ClientThread stp = new ClientThread(server.accept(), game_server.current_game);
 					Thread t = new Thread(stp);
 					t.start();
 				}
